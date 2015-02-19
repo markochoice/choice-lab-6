9ba3cccf9b68874451eb17ddfe3d9dba7f3ec76a
Marko Choice <polo@Markos-MacBook-Pro.local>
2015-02-19 12:54:33 -0500

    Initial Commit

---
+++ choice lab 6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  choice lab 6
+//
+//  Created by Marko Choice on 2/19/15.
+//  Copyright (c) 2015 Marko Choice. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
